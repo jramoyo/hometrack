bebcbe9ee4bab753123ec9ea0a0686a570e15c32
Jan Amoyo <jan.amoyo@siteminder.com>
2017-09-30 00:03:33 +1000

    error handling + tests

--- app/routes.js
+++ app/routes.js
@@ -2,15 +2,49 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const router = express.Router()
 
+const errorcode = require('err-code')
+
 const Service = require('./service')
 const service = new Service()
 
 router.use(bodyParser.json())
 
 router.post('/', (req, res) => {
+  validate(req.body.payload)
+
   res.send({
     response: service.process(req.body.payload)
   })
 })
+
+router.use((err, req, res, next) => {
+  if (!err) next()
+
+  if (err instanceof SyntaxError) { // thrown by body-parser
+    res.status(400).json(error('Could not decode request: JSON parsing failed'))
+  } else if (err.code === '400') {
+    res.status(400).json(error(err.message))
+  } else {
+    next(err)
+  }
+})
+
+const validate = (payload) => {
+  if (!payload) {
+    throw errorcode('Request payload is missing', '400')
+  }
+
+  const everyAddress = payload.reduce((result, value) => {
+    return result && value.address
+  }, true)
+
+  if (!everyAddress) {
+    throw errorcode('Request payload contains an item without an address', '400')
+  }
+}
+
+const error = (message) => {
+  return { error: message }
+}
 
 module.exports = router
--- tests/routes.spec.js
+++ tests/routes.spec.js
@@ -6,13 +6,65 @@ const expect = chai.expect
 
 describe('endpoint', () => {
   describe('POST /', () => {
-    it('does something', () => {
+    it('returns a list items having workflow completed for the type htv', () => {
       return chai.request(app).post('/')
       .send(request)
       .then(res => {
         expect(res).to.have.status(200)
         expect(res).to.be.json
         expect(res.body).to.eql(response)
+      })
+    })
+
+    it('returns an empty list if the payload is empty', () => {
+      return chai.request(app).post('/')
+      .send({ 'payload': [] })
+      .then(res => {
+        expect(res).to.have.status(200)
+        expect(res).to.be.json
+        expect(res.body).to.eql({ 'response': [] })
+      })
+    })
+
+    it('returns 400 if the payload is missing', () => {
+      return chai.request(app).post('/')
+      .send({})
+      .catch(err => {
+        expect(err.response).to.have.status(400)
+        expect(err.response).to.be.json
+        expect(err.response.body).to.eql({
+          'error': 'Request payload is missing'
+        })
+      })
+    })
+
+    it('returns 400 if the payload contains an item without an address', () => {
+      return chai.request(app).post('/')
+      .send({
+        'payload': [{
+          'type': 'htv',
+          'workflow': 'completed'
+        }]
+      })
+      .catch(err => {
+        expect(err.response).to.have.status(400)
+        expect(err.response).to.be.json
+        expect(err.response.body).to.eql({
+          'error': 'Request payload contains an item without an address'
+        })
+      })
+    })
+
+    it('returns 400 if an invalid JSON is received', () => {
+      return chai.request(app).post('/')
+      .set('Content-type', 'application/json')
+      .send('qwerty')
+      .catch(err => {
+        expect(err.response).to.have.status(400)
+        expect(err.response).to.be.json
+        expect(err.response.body).to.eql({
+          'error': 'Could not decode request: JSON parsing failed'
+        })
       })
     })
   })
@@ -146,12 +198,12 @@ const request = {
 const response = {
   'response': [
     {
-      'concataddress': '28 Donington Ave Georges Hall NSW 2198',
+      'concataddress': 'Level 6 146 Arthur Street North Sydney NSW 2060',
       'type': 'htv',
       'workflow': 'completed'
     },
     {
-      'concataddress': '25 Mary St Brisbane QLD 4000',
+      'concataddress': '360 Elizabeth St Melbourne VIC 3000',
       'type': 'htv',
       'workflow': 'completed'
     }
